c5b0a109f4b2b9ae7ea9309c597f33e84c2075ed
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-07-28 21:37:01 +0900

    solve:#4 10250,16401

---
+++ jong4876/10250.cpp
@@ -0,0 +1,34 @@
+﻿//10250
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable:4996)
+
+int main() {
+	int T, H, W, N;
+	int *arr;
+
+	scanf("%d", &T);
+	arr = (int *)malloc(sizeof(int)*T);
+
+	for (int i = 0; i < T; i++) {
+		scanf("%d %d %d", &H, &W, &N);
+
+		int mok = (N - 1) / H; // 호실 뒤번
+		int rest = (N - 1) % H; // 호실 앞번
+
+		int floor = rest + 1;
+		int distance = mok + 1;
+
+		int front = floor * 100;
+
+		arr[i] = front + distance;
+	}
+
+	for (int i = 0; i < T; i++) {
+		printf("%d\n", arr[i]);
+	}
+
+	//system("pause");
+	return 0;
+}
+
---
+++ jong4876/16401.cpp
@@ -0,0 +1,58 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable:4996)
+
+int possible(int *arr, int M, int N, int len){ // 분배가 가능한지
+
+	int cnt = 0;
+	for (int i = 0; i < N; i++) {
+		cnt += arr[i] / len;
+	}
+	if (cnt >= M)
+		return 1;
+	return 0;
+
+}
+int compare(const void *a, const void *b){    // 오름차순 비교 함수 구현
+	int num1 = *(int *)a;
+	int num2 = *(int *)b;   
+
+	if (num1 > num2)    
+		return -1;      
+
+	if (num1 < num2)   
+		return 1;       
+
+	return 0; 
+}
+int main() {
+	int *arr;
+	int N, M;
+	int cnt;
+	int low=0, max;
+
+
+	scanf("%d %d", &M, &N);
+	arr = (int *)malloc(sizeof(int)*N);
+
+	for (int i = 0;i < N;i++) {
+		scanf("%d", &arr[i]);
+	}
+	qsort(arr, N, sizeof(int), compare);
+	max = arr[0];
+
+	int result = 0;
+	while (low <= max){
+		int mid = (low + max) / 2;
+		if (possible(arr,M,N,mid)){
+			result = mid;
+			low = mid + 1;
+		}else
+			max= mid - 1;
+	}
+	printf("%d", result);
+
+	
+	system("pause");
+	return 0;
+}
