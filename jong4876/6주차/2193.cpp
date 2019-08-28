a996dbe39d61f40859dbb510e47c2f30f99bc20c
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-08-28 14:29:26 +0900

    Add files via upload

---
+++ jong4876/6주차/2193.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int N;
+	long arr[91];
+	cin >> N;
+	arr[1] = arr[2] = 1;
+
+	for (int i = 3;i <= N;i++) {
+		arr[i] = arr[i-1] +arr[i-2];
+	}
+
+	cout << arr[N];
+	return 0;
+}
+
+
