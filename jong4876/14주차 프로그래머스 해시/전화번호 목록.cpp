218e17c3d2de826388bb20427b49e3e90f8658c0
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-10-22 14:44:00 +0900

    solve : #19 전화번호 목록
    시작시 정렬하면 반복문 하나 줄임

---
+++ jong4876/14주차 프로그래머스 해시/전화번호 목록.cpp
@@ -0,0 +1,36 @@
+﻿#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+	bool answer = true;
+	
+
+	for (int i = 0;i < phone_book.size();i++) {
+		string destPhone;
+		destPhone = phone_book[i];
+
+		for (int j = 0;j < phone_book.size();j++) {
+			int flag  = 1;
+			if (i == j)
+				continue;
+
+			if (destPhone.length() > phone_book[j].length())
+				continue;
+
+			for (int k = 0;k < destPhone.length();k++) {
+				if (destPhone[k] != phone_book[j][k]) {
+					flag = 0;
+					break;
+				}
+			}
+			if (flag == 1)
+				return false;
+		}
+	}
+
+
+	return answer;
+}
