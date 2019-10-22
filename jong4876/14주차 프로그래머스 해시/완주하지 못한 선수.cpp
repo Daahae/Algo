292ead3785c60fd565d82ece5ccdce819b413cc2
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-10-22 14:09:19 +0900

    solve : #19 완주하지 못한 선수

---
+++ jong4876/14주차 프로그래머스 해시/완주하지 못한 선수.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+	string answer = "";
+	map<string, int> marathonMap;
+
+
+	for (int i = 0;i < participant.size(); i++)
+		marathonMap[participant[i]] = 0;
+
+	for (int i = 0;i < participant.size(); i++)
+		marathonMap[participant[i]]--;
+
+
+	for (int i = 0;i < completion.size(); i++)
+		marathonMap[completion[i]]++;
+
+	for (int i = 0;i < participant.size(); i++)
+		cout <<participant[i]<< " : "<< marathonMap[participant[i]]<<endl;
+
+
+	for (int i = 0;i < participant.size(); i++) {
+		if (marathonMap[participant[i]] < 0) {
+			answer = participant[i];
+		}
+	}
+	return answer;
+
+}
