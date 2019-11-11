9abac5fbdde8f84cbceb349232966fb440cd8c1d
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-11-11 18:48:20 +0900

    incomplete solve

--- jong4876/15주차 프로그래머스/큰 수 만들기.cpp
+++ jong4876/15주차 프로그래머스/큰 수 만들기.cpp
@@ -1,4 +1,4 @@
-﻿#include <string>
+#include <string>
 #include <vector>
 #include <algorithm>
 #include <iostream>
@@ -12,34 +12,81 @@ bool compare(char a, char b) {
 string solution(string number, int k) {
 	string answer = "";
 	vector<char> stringNum;
-	vector<int> intIdx;
+	vector<int> edgeIdx;
+	int startIdx; // 최댓값 찾기를 실시할 시작 인덱스
+	char max = -1;
 
 	for (int i = 0;i < number.length();i++)
 		stringNum.push_back(number[i]);
 
-	sort(stringNum.begin(), stringNum.end());
+	for (int i = 0;i < number.length() - k+1;i++) { // 뒤에서 N-k이상떨어져 있고 시작할 최댓값
+		if (max < number[i]) {
+			max = number[i];
+			startIdx = i;
+		}
+	}
 
-	for (int i = 0;i < stringNum.size();i++)
-		cout << stringNum[i] << endl;
-		
-	int cnt = 0;
-	while (1) {
+
+	k = k - startIdx;
+	
+
+	for (int i = startIdx;i < number.length() - k;i++) { // 퐁당퐁일때 빼기
+		if (i == startIdx)
+			continue;
+
 		if (k == 0)
 			break;
-		for (int i = 0;i < number.length();i++) {
-			if (stringNum[cnt] == number[i]) {
-				number.erase(number.begin() + i);		
-				k--;
-				if (k == 0)
-					break;
+
+		if ((number[i - 1] > number[i]) && (number[i] < number[i + 1])) {
+			edgeIdx.push_back(i);
+			k--;
+		}
+	}
+
+	int cnt = 0;
+	if (edgeIdx.size() == 0) { //반례추가
+		for (int i = startIdx;i < number.length();i++)
+			answer += number[i];
+
+		while (k > 0) {
+			int min = -1;
+			int minIdx;
+			for (int i = 0;i < answer.length();i++) {
+				if (min > answer[i]) {
+					min = answer[i];
+					minIdx = i;
+				}
+			}
+			answer.erase(answer.begin() + minIdx);
+			k--;
+		}
+
+
+		return answer;
+	}
+
+	for (int i = startIdx;i < number.length();i++) { // 앞에 제거한값 대입
+		if (edgeIdx[cnt] == i && (edgeIdx.size()-1 >= cnt)) {
+			int idx = edgeIdx[cnt];
+			cnt++;
+			continue;
+		}
+
+		answer += number[i];
+	}
+
+	while (k > 0) {
+		int min = -1;
+		int minIdx;
+		for (int i = 0;i < answer.length();i++) {
+			if (min > answer[i]) {
+				min = answer[i];
+				minIdx = i;
 			}
 		}
-		cnt++;
+		answer.erase(answer.begin() + minIdx);
+		k--;
 	}
-	for (int i = 0;i < number.length();i++)
-		answer += number[i];
-
-	
 
 	return answer;
 }
