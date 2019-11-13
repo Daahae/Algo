8ad860c63cf74af74e09baf245f83c74033f94c2
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-11-13 21:43:10 +0900

    incomplete solve : #20 디스크 컨트롤러

---
+++ jong4876/15주차 프로그래머스/디스크 컨트롤러.cpp
@@ -0,0 +1,67 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+bool compare(const pair<int, int> &a, const pair<int, int> &b) {
+	
+	if (a.second == b.second)
+		return a.first < b.first;
+
+	else
+		return a.second < b.second;
+}
+
+
+int calculateAvg(vector<pair<int, int>> pairJobs) {
+	queue<pair<int, int>> Q;
+	int resSum = 0;
+	int presentIdx = 0;
+	int jobsSize = pairJobs.size();
+
+	for (int i = 0;i < pairJobs.size();i++)
+		Q.push(pairJobs[i]);
+
+	while (!Q.empty()) { // resSum  3 ,  before  3
+		resSum += Q.front().second;
+
+		if(Q.front().first >= presentIdx)
+			presentIdx = Q.front().first + Q.front().second;
+
+		else {
+			int diff = presentIdx - Q.front().first;
+			resSum += diff;
+			presentIdx = Q.front().first + Q.front().second+ diff;
+		}
+
+		Q.pop();
+	}
+
+	
+
+
+	return resSum / jobsSize;
+
+}
+
+
+
+int solution(vector<vector<int>> jobs) {
+	int answer = 0;
+	vector<pair<int, int>> pairJobs;
+
+
+	for (int i = 0;i < jobs.size();i++) {
+		pairJobs.push_back(pair<int, int>(jobs[i][0], jobs[i][1]));
+	}
+
+
+	sort(pairJobs.begin(), pairJobs.end(), compare);
+
+	answer = calculateAvg(pairJobs);
+
+
+	return answer;
+}
