cb5499da2e10665febd137c16cad78af9467a7cb
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-11-18 18:32:44 +0900

    Update 순위.cpp

--- jong4876/16주차 프로그래머스 순위, 저울/순위.cpp
+++ jong4876/16주차 프로그래머스 순위, 저울/순위.cpp
@@ -1,12 +1,28 @@
-﻿#include <string>
+#include <string>
 #include <vector>
 #include <queue>
 #include <iostream>
 
 using namespace std;
-int playerArr[100][100] = { -1, };
+int playerArr[101][101];
+int finnishPlayer[101]; //체크 필요: 0 체크 필요X: 1
 
-void addExtraPlay(int n,int presentPlayer, int versusPlayer, int result) {
+void addExtraPlay(int n, int presentPlayer, int versusPlayer, int result) {
+	// 유추할 수 있는 경기결과를 추가함
+
+	int cnt = 0;
+
+	if (finnishPlayer[presentPlayer] == 1)
+		return;
+
+	for (int i = 1;i <= n;i++) {
+		if (playerArr[presentPlayer][i] != 0)
+			cnt++;
+	}
+	if (n - 1 == cnt) { // 검사끝난건 체크할필요없음
+		finnishPlayer[presentPlayer] = 1;
+		return;
+	}
 
 	for (int i = 1;i <= n;i++) {
 		if (playerArr[versusPlayer][i] == result) {
@@ -18,59 +34,30 @@ void addExtraPlay(int n,int presentPlayer, int versusPlayer, int result) {
 }
 
 
-int solution(int n, vector<vector<int>> results) {// 선수의 수 n
+int solution(int n, vector<vector<int>> results) {
+	// 선수의 수 n
 	int answer = 0;
 	for (int i = 0;i < results.size();i++) {
 		int winner = results[i][0];
 		int loser = results[i][1];
 
 		playerArr[winner][loser] = 1;
-		playerArr[loser][winner] = -1; 
+		playerArr[loser][winner] = -1;
 	}
-	// 플로이드 와샬로 추가정보 캐치?
-	// player 이름은 12345순?
+	// 플로이드 와샬로 추가정보 캐치
+	// *** 상대한테 이겼다는건 상대가 이긴 상대는 다이김 / 졌다는건 상대의 진 상대는 다짐
 
-	for (int i = 1;i <= n;i++) {
-		for (int j = 1;j <= n;j++) {
-			cout << playerArr[i][j] << " ";
-		}
-		cout << endl;
-	}
+	for (int i = 0;i < results.size();i++) {
+		int winner = results[i][0];
+		int loser = results[i][1];
 
-	
-	for (int i = 1;i <= n;i++) { // floyd -warshall
-		// 상대한테 이겼다는건 상대가 이긴 상대는 다이김 / 졌다는건 상대의 진 상대는 다짐
-		for (int j = 1;j <= n;j++) {
-			if (i == j)
-				continue;
-
-			if (playerArr[i][j] != -1) {
-				//addExtraPlay(n, i, j, playerArr[i][j]);
-			}
-		}
+		addExtraPlay(n, winner, loser, playerArr[winner][loser]);
+		addExtraPlay(n, loser, winner, playerArr[loser][winner]);
 	}
 
 
-
-	for (int i = 1;i <= n;i++) {
-		int rank = 0;
-		int presentPlayer = i;
-		int minusCnt = 1;
-		for (int j = 1;j <= n;j++) {
-			if (i == j)
-				continue;
-
-			if (playerArr[presentPlayer][j] == 0) {
-				minusCnt = -1;
-				break;
-			}
-		}
-		if (minusCnt == 1)
-			answer++;
-
-	}
-
-
+	for (int i = 1;i <= n;i++)
+		answer += finnishPlayer[i];
 
 
 	return answer;
