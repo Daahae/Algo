bd609175f1c7084a3a117fe386c0af644012932e
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-11-20 01:12:43 +0900

    incomplete solve : #21 저울
    dp 미사용

---
+++ jong4876/16주차 프로그래머스 순위, 저울/저울.cpp
@@ -0,0 +1,42 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int dp[10000000]; //  시간오바시 확인
+
+int solution(vector<int> weight) {
+	int answer = 0;
+
+	sort(weight.begin(), weight.end());
+
+	while (1) {
+		answer++;
+		int tmpAnswer = answer;
+		int sum = 0;
+		int maxUnderAnswer;
+		int idx;
+		for (int i = 1;i < weight.size();i++) {
+			if (weight[i] >= answer) {
+				maxUnderAnswer = weight[i - 1];
+				idx = i;
+				break;
+			}
+		}
+		
+		int successFlag = 0; //  저울추로 측정 가능한지 여부
+		for (int i = idx; i >= 0;i--) {
+			if (tmpAnswer - weight[i] < 0)
+				continue;
+
+			tmpAnswer -= weight[i];
+			if (tmpAnswer == 0) {
+				successFlag = 1;
+				break;
+			}
+		}
+		if (successFlag == 0)
+			return answer;
+	}
+}
