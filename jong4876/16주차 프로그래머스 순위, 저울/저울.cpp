9249a546754d9d7779c88ff01f14d8c8c2192b08
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-11-20 01:23:51 +0900

    Update 저울.cpp
    정확성 100, 효율성 0

--- jong4876/16주차 프로그래머스 순위, 저울/저울.cpp
+++ jong4876/16주차 프로그래머스 순위, 저울/저울.cpp
@@ -1,15 +1,18 @@
-﻿#include <string>
+#include <string>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int dp[10000000]; //  시간오바시 확인
+int dp[1000000]; //  시간오바시 확인
 
 int solution(vector<int> weight) {
 	int answer = 0;
 
 	sort(weight.begin(), weight.end());
+
+	for (int i = 0;i < weight.size();i++)
+		dp[weight[i]] = 1; // 만들 수 있는 무게값 1체크
 
 	while (1) {
 		answer++;
@@ -21,20 +24,21 @@ int solution(vector<int> weight) {
 			if (weight[i] >= answer) {
 				maxUnderAnswer = weight[i - 1];
 				idx = i;
-				break;
 			}
 		}
 		
 		int successFlag = 0; //  저울추로 측정 가능한지 여부
 		for (int i = idx; i >= 0;i--) {
-			if (tmpAnswer - weight[i] < 0)
+			if (tmpAnswer - weight[i] < 0) {			
 				continue;
+			}
 
 			tmpAnswer -= weight[i];
 			if (tmpAnswer == 0) {
 				successFlag = 1;
 				break;
 			}
+
 		}
 		if (successFlag == 0)
 			return answer;
