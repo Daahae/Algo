73b249dfa8cc116c645f828d986b9a77ba91d93f
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-11-21 20:13:55 +0900

    Update 저울.cpp

--- jong4876/16주차 프로그래머스 순위, 저울/저울.cpp
+++ jong4876/16주차 프로그래머스 순위, 저울/저울.cpp
@@ -11,25 +11,15 @@ int solution(vector<int> weight) {
 
 	sort(weight.begin(), weight.end());
 
-	for (int i = 0;i < weight.size();i++)
-		dp[weight[i]] = 1; // 만들 수 있는 무게값 1체크
-
 	while (1) {
 		answer++;
 		int tmpAnswer = answer;
 		int sum = 0;
 		int maxUnderAnswer;
-		int idx;
-		for (int i = 1;i < weight.size();i++) {
-			if (weight[i] >= answer) {
-				maxUnderAnswer = weight[i - 1];
-				idx = i;
-			}
-		}
-		
+
 		int successFlag = 0; //  저울추로 측정 가능한지 여부
-		for (int i = idx; i >= 0;i--) {
-			if (tmpAnswer - weight[i] < 0) {			
+		for (int i = weight.size() -1 ; i >= 0;i--) {
+			if (tmpAnswer - weight[i] < 0) {
 				continue;
 			}
 
