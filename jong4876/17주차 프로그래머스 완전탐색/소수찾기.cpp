7c0f09119c26b3e7f77b387a79b271605d74bbd6
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-11-23 08:57:44 +0900

    sovle : #22 소수찾기
    next_permutation, map

---
+++ jong4876/17주차 프로그래머스 완전탐색/소수찾기.cpp
@@ -0,0 +1,56 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <map>
+
+using namespace std;
+map<int, int> numMap; // map을 통해 중복제거
+
+bool isDecimal(int num) {
+	if (num == 0 || num == 1)
+		return false;
+
+
+	for (int i = 2;i < num;i++) {
+		if (num % i == 0)
+			return false;
+	}
+
+	return true;
+}
+
+void countDecimal() {
+
+}
+
+int solution(string numbers) {
+	int answer = 0;
+	
+	
+	sort(numbers.begin(), numbers.end());
+
+	for (int i = 1;i <= numbers.length();i++) {
+		string strNum;
+		for (int j = 0;j < i;j++) {
+			strNum.push_back(numbers[j]);
+		}
+		int num = atoi(strNum.c_str());
+		if (isDecimal(num))
+			numMap.insert(pair<int, int>(num, 0));
+
+		while (next_permutation(numbers.begin(), numbers.end())) {
+			string strNum;
+			for (int j = 0;j < i;j++) {
+				strNum.push_back(numbers[j]);
+			}
+			num = atoi(strNum.c_str());
+			if (isDecimal(num))
+				numMap.insert(pair<int, int>(num, 0));
+		}
+	}
+	
+
+
+	return numMap.size();
+}
