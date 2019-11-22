c181f91988a7dcebaac944de93ea6f296f725c06
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-11-22 14:20:11 +0900

    solve : #21 저울
    weight[i+1] < sum[i] +1 조건이라면 sum[i] 이전의 값들을 전부 만들 수 있음

--- jong4876/16주차 프로그래머스 순위, 저울/저울.cpp
+++ jong4876/16주차 프로그래머스 순위, 저울/저울.cpp
@@ -4,33 +4,16 @@
 
 using namespace std;
 
-int dp[1000000]; //  시간오바시 확인
-
 int solution(vector<int> weight) {
 	int answer = 0;
+	int sum = 0;
 
 	sort(weight.begin(), weight.end());
 
-	while (1) {
-		answer++;
-		int tmpAnswer = answer;
-		int sum = 0;
-		int maxUnderAnswer;
-
-		int successFlag = 0; //  저울추로 측정 가능한지 여부
-		for (int i = weight.size() -1 ; i >= 0;i--) {
-			if (tmpAnswer - weight[i] < 0) {
-				continue;
-			}
-
-			tmpAnswer -= weight[i];
-			if (tmpAnswer == 0) {
-				successFlag = 1;
-				break;
-			}
-
-		}
-		if (successFlag == 0)
-			return answer;
+	for (int i = 0;i < weight.size() -1 ;i++) {
+		sum += weight[i];
+		if (weight[i + 1] > sum + 1)
+			return sum + 1;
 	}
+	return sum + weight[weight.size()-1]+1;
 }
