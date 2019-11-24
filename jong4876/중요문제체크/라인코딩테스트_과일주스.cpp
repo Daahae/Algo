b32e15d53f904c882e8abe20b0c96dbec645acae
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-11-24 15:38:21 +0900

    Add files via upload
    stack queue 사용을 좀더 신중히 생각해서 접근했어야 함

---
+++ jong4876/중요문제체크/라인코딩테스트_과일주스.cpp
@@ -0,0 +1,56 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> heightList) {
+	int nResult = 0;
+	int flag = 0; // 0 감소, 1 증가
+	int calculateCnt = 0;
+	queue<int> Q;
+
+	for (int i = 0;i < heightList.size();i++) {
+		if (Q.empty()) {
+			Q.push(heightList[i]);
+			continue;
+		}
+
+		// 증가일 때 Q안에 있는 너비들 계산
+		if (Q.front() <= heightList[i]) { 
+			if (Q.size() == 1) {
+				Q.pop();
+			}
+			else { // 스택에 2이상
+				int minHeight = min(Q.front(), heightList[i]);
+				while (!Q.empty()) {
+					if (minHeight - Q.front() > 0)
+						nResult += (minHeight - Q.front());
+					Q.pop();
+				}
+			}
+		}
+		Q.push(heightList[i]);
+	}
+	return nResult;
+}
+
+
+int main() {
+	vector<int> heightList;
+	int N;
+
+	cin >> N;
+
+	for (int i = 0;i < N;i++) {
+		int tmp;
+		cin >> tmp;
+		heightList.push_back(tmp);
+	}
+
+	cout << solution(heightList)<<endl;
+
+	return 0;
+}
