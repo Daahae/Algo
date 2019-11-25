0debad787529e60fc460ee8ac9b2cca3c1fc3e2c
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-11-25 20:03:14 +0900

    solve : #22 완전탐색

---
+++ jong4876/17주차 프로그래머스 완전탐색/수포자.cpp
@@ -0,0 +1,61 @@
+﻿#include <string>
+#include <vector>
+#include<algorithm>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> answers) {
+	vector<int> answer;
+	vector<int> answerCntByPlayer;
+	vector<int> player1 = {1,2,3,4,5};
+	vector<int> player2 = {2,1,2,3,2,4,2,5};
+	vector<int> player3 = {3,3,1,1,2,2,4,4,5,5};
+	int answerCnt1 = 0;
+	int answerCnt2 = 0;
+	int answerCnt3 = 0;
+	//12345
+	//21232425
+	//3311224455
+	
+	for (int i = 0;i < answers.size();i++) {
+	
+		int idx = (i % player1.size());
+
+	
+		if (answers[i] == player1[idx]) {
+			answerCnt1++;
+		}
+	
+		idx = (i % player2.size());
+		if (answers[i] == player2[idx]) {
+			answerCnt2++;
+		}
+
+		
+		idx = (i % player3.size());
+		if (answers[i] == player3[idx]) {
+			answerCnt3++;
+		}
+	}
+	answerCntByPlayer.push_back(answerCnt1);
+	answerCntByPlayer.push_back(answerCnt2);
+	answerCntByPlayer.push_back(answerCnt3);
+
+
+
+	int max = -1;
+	for (int i = 0;i < 3;i++) {
+		cout << answerCntByPlayer[i];
+		if (max < answerCntByPlayer[i])
+			max = answerCntByPlayer[i];
+
+	}
+	for (int i = 0;i < 3;i++) {
+		if (max == answerCntByPlayer[i])
+			answer.push_back(i + 1);
+	}
+
+
+	return answer;
+}
