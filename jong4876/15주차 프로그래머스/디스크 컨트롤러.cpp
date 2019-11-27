ac43ba4c8210f2a16e392633db4320c7f836288c
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-11-27 20:41:24 +0900

    Update 디스크 컨트롤러.cpp

--- jong4876/15주차 프로그래머스/디스크 컨트롤러.cpp
+++ jong4876/15주차 프로그래머스/디스크 컨트롤러.cpp
@@ -4,9 +4,6 @@
 #include <iostream>
 using namespace std;
 
-int workTime = 0;
-vector<pair<int, int>> primaryQ; //들어온시점, 걸린 시간
-
 bool compare(const pair<int, int> &a, const pair<int, int> &b) {
 	
 	return a.second < b.second;
@@ -14,41 +11,37 @@ bool compare(const pair<int, int> &a, const pair<int, int> &b) {
 }
 // 들어온 시간순으로 수행하되, 비는 시간에는 최소 시간을 집어넣음
 
-void printQ() {
-	for (int i = 0;i < primaryQ.size();i++)
-		cout << primaryQ[i].second << endl;
-}
-
 int solution(vector<vector<int>> jobs) {
 	int answer = 0;
 	int size;
 	int timeSum = 0;
+	int presentTime = 0;
+	vector<pair<int, int>> primaryQ; //들어온시점, 걸린 시간
 
-	for (int i = 0;i < jobs.size();i++) {
-		primaryQ.push_back(pair<int, int>(jobs[i][0], jobs[i][1]));
-	}
-	size = primaryQ.size();
 
+	// 프로세스의 작업시간 기준으로 오름차순 정렬
+	// -> 우선순위 큐 처럼 사용
+
+	for (int i = 0;i < jobs.size();i++) 
+		primaryQ.push_back(pair<int, int>(jobs[i][0], jobs[i][1])); 
 	sort(primaryQ.begin(), primaryQ.end(), compare);
 
-
-	while (primaryQ.size() > 0) {
+	while (primaryQ.size() > 0) { // 프로세스를 다 실행시킬 때까지
 		int i;
 		int flag = 0;
 		
 		for (i = 0;i < primaryQ.size();i++) {
-			if (workTime >= primaryQ[i].first) { // 유휴시간 없이 대기하는 프로세스가 있다면
-				workTime += primaryQ[i].second; // 결국에 총 걸리는 시간은 second의 총합임을 인지
-				timeSum += (workTime - primaryQ[i].first); // 딜레이 된 시간 합
-				primaryQ.erase(primaryQ.begin() + i);		
+			if (presentTime >= primaryQ[i].first) { // 유휴시간 없이 대기하는 프로세스가 있다면
+				presentTime += primaryQ[i].second; // 바로 해당프로세스를 실행
+				timeSum += (presentTime - primaryQ[i].first); // 딜레이 된 시간 역시 더해줌
+				primaryQ.erase(primaryQ.begin() + i); // 실행된 프로세스 제거
 				flag = 1;
 				break;
 			}
 		}
-		if(flag == 0)
-			workTime++;
+		if(flag == 0) // 해당presentTime 에 실행할 수 있는 프로세스가 없다면
+			presentTime++;  // 무의미하게 시간을 보냄, 어쩔수없음
 	}
 	answer = timeSum / size;
-	
 	return answer;
 }
