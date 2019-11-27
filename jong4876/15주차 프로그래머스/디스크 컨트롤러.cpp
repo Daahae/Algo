568b0f37dfa552ea196b313ea059d13a42e556fb
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-11-27 20:35:55 +0900

    solve : 디스크 컨트롤러
    Think with draw

--- jong4876/15주차 프로그래머스/디스크 컨트롤러.cpp
+++ jong4876/15주차 프로그래머스/디스크 컨트롤러.cpp
@@ -1,67 +1,54 @@
-﻿#include <string>
+#include <string>
 #include <vector>
 #include <algorithm>
-#include <queue>
-
+#include <iostream>
 using namespace std;
+
+int workTime = 0;
+vector<pair<int, int>> primaryQ; //들어온시점, 걸린 시간
 
 bool compare(const pair<int, int> &a, const pair<int, int> &b) {
 	
-	if (a.second == b.second)
-		return a.first < b.first;
-
-	else
-		return a.second < b.second;
-}
-
-
-int calculateAvg(vector<pair<int, int>> pairJobs) {
-	queue<pair<int, int>> Q;
-	int resSum = 0;
-	int presentIdx = 0;
-	int jobsSize = pairJobs.size();
-
-	for (int i = 0;i < pairJobs.size();i++)
-		Q.push(pairJobs[i]);
-
-	while (!Q.empty()) { // resSum  3 ,  before  3
-		resSum += Q.front().second;
-
-		if(Q.front().first >= presentIdx)
-			presentIdx = Q.front().first + Q.front().second;
-
-		else {
-			int diff = presentIdx - Q.front().first;
-			resSum += diff;
-			presentIdx = Q.front().first + Q.front().second+ diff;
-		}
-
-		Q.pop();
-	}
-
-	
-
-
-	return resSum / jobsSize;
+	return a.second < b.second;
 
 }
+// 들어온 시간순으로 수행하되, 비는 시간에는 최소 시간을 집어넣음
 
-
+void printQ() {
+	for (int i = 0;i < primaryQ.size();i++)
+		cout << primaryQ[i].second << endl;
+}
 
 int solution(vector<vector<int>> jobs) {
 	int answer = 0;
-	vector<pair<int, int>> pairJobs;
-
+	int size;
+	int timeSum = 0;
 
 	for (int i = 0;i < jobs.size();i++) {
-		pairJobs.push_back(pair<int, int>(jobs[i][0], jobs[i][1]));
+		primaryQ.push_back(pair<int, int>(jobs[i][0], jobs[i][1]));
 	}
+	size = primaryQ.size();
+
+	sort(primaryQ.begin(), primaryQ.end(), compare);
 
 
-	sort(pairJobs.begin(), pairJobs.end(), compare);
-
-	answer = calculateAvg(pairJobs);
-
-
+	while (primaryQ.size() > 0) {
+		int i;
+		int flag = 0;
+		
+		for (i = 0;i < primaryQ.size();i++) {
+			if (workTime >= primaryQ[i].first) { // 유휴시간 없이 대기하는 프로세스가 있다면
+				workTime += primaryQ[i].second; // 결국에 총 걸리는 시간은 second의 총합임을 인지
+				timeSum += (workTime - primaryQ[i].first); // 딜레이 된 시간 합
+				primaryQ.erase(primaryQ.begin() + i);		
+				flag = 1;
+				break;
+			}
+		}
+		if(flag == 0)
+			workTime++;
+	}
+	answer = timeSum / size;
+	
 	return answer;
 }
