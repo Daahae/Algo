32532f7227e6f7d2680fb6f7e228c95c8034759c
rkddlsgur983 <rkddlsgur983@naver.com>
2019-12-04 22:11:42 +0900

    feat: #23 Solve 동적계획법

---
+++ rkddlsgur983/Programmers/18주차_동적계획법/2_정수삼각형.cpp
@@ -0,0 +1,28 @@
+// dp 정수 삼각형
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> triangle) {
+    int answer = 0;
+    vector<vector<int>> dp(triangle.size(), vector<int>(triangle.size()));
+    
+    dp[0][0] = triangle[0][0];
+    for (int i = 1; i < triangle.size(); i++) {
+        for (int j = 0; j <= i; j++) {
+            if (j == 0) {
+                dp[i][j] = dp[i-1][j] + triangle[i][j];
+            } else if (j == i) {
+                dp[i][j] = dp[i-1][j-1] + triangle[i][j];
+            } else {
+                dp[i][j] = max(dp[i-1][j-1] + triangle[i][j], dp[i-1][j] + triangle[i][j]);
+            }
+            if (i == triangle.size()-1) {
+                answer = max(answer, dp[i][j]);
+            }
+        }
+    }
+    return answer;
+}
