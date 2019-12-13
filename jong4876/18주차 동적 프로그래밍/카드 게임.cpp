165bf99b40f3090821b3cfbe63af820aefe3dd51
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-12-13 22:12:13 +0900

    incomplete solve: #23 카드 게임
    55점

---
+++ jong4876/18주차 동적 프로그래밍/카드 게임.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> left, vector<int> right) {
+	int answer = 0;
+	int Lidx = 0, Ridx = 0;
+	int maxSize = left.size(); // 양쪽 사이즈 같음
+
+	while (1) {
+		if ((Lidx == left.size()) || (Ridx == right.size()))
+			break;
+
+		if (left[Lidx] > right[Ridx]) {
+
+			answer += right[Ridx];
+			Ridx++;
+		}
+		else {
+			Lidx++;
+		}
+	}
+
+
+
+	return answer;
+}
