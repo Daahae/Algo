657d9bbb1576792bc7645e349705b1b713a3ade1
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-12-14 20:11:40 +0900

    Update 도둑질.cpp

--- jong4876/18주차 동적 프로그래밍/도둑질.cpp
+++ jong4876/18주차 동적 프로그래밍/도둑질.cpp
@@ -1,23 +1,31 @@
-﻿#include <string>
+#include <string>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int dp[1000000];
+int dp1[1000000];// 처음 집을 터는경우
+int dp2[1000000]; // 안터는 경우
 
 int solution(vector<int> money) {
 	int answer = 0;
 
 
-	dp[0] = money[0];
-	dp[1] = money[1];
+	dp1[0] = money[0];
+	dp1[1] = money[0]; // 처음집 털면 마지막 집 못텀
+
+	dp2[0] = 0;
+	dp2[1] = money[1];
+	dp2[2] = money[1]; // 처음집을 안털면 마지막집 털수 있음
 
 	for (int i = 2;i < money.size();i++) {
-		dp[i] = max(money[i] + dp[i - 2], dp[i - 1]);
+		dp1[i] = max(money[i] + dp1[i - 2], dp1[i - 1]);
 	}
 
+	for (int i = 3;i < money.size();i++) {
+		dp2[i] = max(money[i] + dp2[i - 2], dp2[i - 1]);
+	}
 
-
-	return dp[money.size()-1];
+	int maxVal = max(dp1[money.size() - 2], dp2[money.size() - 1]);
+	return maxVal;
 }
