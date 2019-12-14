b8ae717368c02340af9144dd4494f392755ca551
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-12-14 19:26:28 +0900

    incomplete solve : #23 도둑질
    45points

---
+++ jong4876/18주차 동적 프로그래밍/도둑질.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int dp[1000000];
+
+int solution(vector<int> money) {
+	int answer = 0;
+
+
+	dp[0] = money[0];
+	dp[1] = money[1];
+
+	for (int i = 2;i < money.size();i++) {
+		dp[i] = max(money[i] + dp[i - 2], dp[i - 1]);
+	}
+
+
+
+	return dp[money.size()-1];
+}
