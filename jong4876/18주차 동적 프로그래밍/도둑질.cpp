f597e84017a847d16419311d86301ca8f639edd9
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-12-14 20:23:32 +0900

    solve : #23 도둑질
    조건에 유의

--- jong4876/18주차 동적 프로그래밍/도둑질.cpp
+++ jong4876/18주차 동적 프로그래밍/도둑질.cpp
@@ -4,8 +4,8 @@
 
 using namespace std;
 
-int dp1[1000000];// 처음 집을 터는경우
-int dp2[1000000]; // 안터는 경우
+int dp1[1000001];// 처음 집을 터는경우
+int dp2[1000001]; // 처음 집 안터는 경우
 
 int solution(vector<int> money) {
 	int answer = 0;
@@ -16,16 +16,20 @@ int solution(vector<int> money) {
 
 	dp2[0] = 0;
 	dp2[1] = money[1];
-	dp2[2] = money[1]; // 처음집을 안털면 마지막집 털수 있음
+	// 처음집을 안털면 마지막집 털수 있음
+	// 첫집을 안턴다고 2번째를 꼭 털필요도 없음 ** [5,2,9]
 
 	for (int i = 2;i < money.size();i++) {
 		dp1[i] = max(money[i] + dp1[i - 2], dp1[i - 1]);
 	}
 
-	for (int i = 3;i < money.size();i++) {
+	for (int i = 2;i < money.size();i++) {
 		dp2[i] = max(money[i] + dp2[i - 2], dp2[i - 1]);
 	}
 
 	int maxVal = max(dp1[money.size() - 2], dp2[money.size() - 1]);
+	// dp1 : 처음 집 털면 마지막 집 터는 경우는 뺴야되서 -2
+	// dp2 : 마지막 집 터는게 가능해서 -1
+
 	return maxVal;
 }
