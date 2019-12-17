ca180f0d057048310ac85cad3e6ab9097b6dbad8
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-12-18 00:07:56 +0900

    incomplete solve : #24 더 맵게
    효율성 X

---
+++ jong4876/19주차 프로그래머스 힙/더 맵게.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+	int answer = 0;
+	int firstIdx = 0;
+
+	sort(scoville.begin(), scoville.end());
+	while (scoville[firstIdx] < K ) {
+		if (firstIdx >= scoville.size()) {
+			answer = -1;
+			break;
+		}
+
+		scoville[firstIdx+1] = scoville[firstIdx] + scoville[firstIdx+1]*2;
+		sort(scoville.begin() + firstIdx + 1, scoville.end());
+		firstIdx++;
+	}
+
+
+	return answer;
+}
