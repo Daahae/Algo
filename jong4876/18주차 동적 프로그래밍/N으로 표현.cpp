f6246a852622dce08d0e7bba1fca53addbdab75e
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-12-22 21:38:17 +0900

    incomplete solve : #23 N으로 표현

---
+++ jong4876/18주차 동적 프로그래밍/N으로 표현.cpp
@@ -0,0 +1,47 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+vector<int> vec;
+
+int startNum;
+int minCnt = 999999;
+int dp[33000]; // 인덱스 number, 값 cnt
+
+void calCount(int N, int number, int cnt) {
+
+	if (cnt > 9)
+		return;
+
+	if (N == number) {
+		minCnt = min(minCnt, cnt);
+		return;
+	}
+
+	//dp[N] = cnt; // number를 만들 수 있는 경우의 수 cnt
+
+	
+	calCount(N*10 - N, number, cnt + 1);
+
+	calCount(N +startNum, number, cnt + 1);
+	
+	calCount(N+ 1, number, cnt + 1); // startNum / startNum = 1
+	
+	calCount(N / startNum, number, cnt + 1);
+
+	
+}
+
+
+int solution(int N, int number) {
+	int answer = 0;
+	
+	startNum = N;
+	calCount(N, number,1);
+
+	if (minCnt == 999999)
+		minCnt = -1;
+
+	return minCnt;
+}
