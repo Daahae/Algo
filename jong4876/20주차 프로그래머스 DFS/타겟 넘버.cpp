668ac702476f4372b8ba9c5be98cc79e1768e1b3
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-12-24 20:56:04 +0900

    solve : #24 타겟 넘버
    음 양의 모든 경우의 수 순환

---
+++ jong4876/20주차 프로그래머스 DFS/타겟 넘버.cpp
@@ -0,0 +1,35 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int totalCnt = 0;
+int visited[21]; // 0 none visited / 1 visited
+
+void dfs(vector<int> numbers, int target, int idx, int sum) {
+
+	int sum = 0;
+
+	if (sum == target && idx == numbers.size()) {
+		totalCnt++;
+		return;
+	}
+	if (idx == numbers.size())
+		return;
+
+
+	
+	dfs(numbers, target, idx+1, sum+numbers[idx]);
+	dfs(numbers, target, idx+1, sum-numbers[idx]);
+
+}
+
+
+int solution(vector<int> numbers, int target) {
+	
+	dfs(numbers, target, 0,0);
+
+
+	return totalCnt;
+}
