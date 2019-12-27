63cc1435bde023e1d0fa63e3318b9e3dd56aab9b
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-12-27 17:43:42 +0900

    solve : #25 빗물 추가
    한 인덱스에서 얻을 수 있는 빗물의 양 단위로 구하기

---
+++ jong4876/20주차 백준 빗물.cpp
@@ -0,0 +1,58 @@
+﻿#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> block;
+
+int getMaxHeightIdx(int startIdx, int endIdx) {
+
+	int maxVal = 0;
+	int maxIdx = startIdx;
+	for (int i = startIdx; i < endIdx;i++) {
+		if (maxVal < block[i]) {
+			maxVal = block[i];
+			maxIdx = i;
+
+		}
+	}
+	return maxIdx;
+	
+}
+
+int main() {
+
+	int H, W;
+	int answer = 0;
+
+	cin >> H >> W;
+
+
+	for (int i = 0;i < W;i++) {
+		int height;
+		cin >> height;
+		block.push_back(height);
+	}
+
+	int leftIdx = 0;
+	int rightIdx;
+	int i = 0;
+
+	// 해당 인덱스에 담길수 있는 물의 높이를 더해나감
+
+	for(int i =0;i<W;i++){ 
+		int leftIdx = getMaxHeightIdx(0, i+1); // 미만을 인덱스로 지정했기때문 0~i
+		int rightIdx = getMaxHeightIdx(i, W); // i ~ W-1
+		
+		int axis = min(block[leftIdx], block[rightIdx]);
+		answer += axis - block[i];
+	}
+
+
+	cout << answer;
+
+	return 0;
+}
