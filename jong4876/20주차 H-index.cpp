667c97c0ff5c045649e8b543961afc218a891004
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2019-12-28 16:39:40 +0900

    solve : #25 H-index
    문제를 제대로 이해, greater<int> : 내림차순 *

---
+++ jong4876/20주차 H-index.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+
+int solution(vector<int> citations) {
+	int h = 0;
+	sort(citations.begin(), citations.end(), greater<int>());
+
+
+	int overCnt = 0; // h번 이상 인용된 논문 수
+	for (int i = 0;i < citations.size();i++) {
+		if (citations[i] <= h)
+			break;
+		
+		
+		h++;
+	}
+	return h;
+
+}
