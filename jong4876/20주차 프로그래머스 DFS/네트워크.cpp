4a2cb3e1824065ebd59d70aebee25707bce36301
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2020-01-01 22:47:36 +0900

    Update 네트워크.cpp

--- jong4876/20주차 프로그래머스 DFS/네트워크.cpp
+++ jong4876/20주차 프로그래머스 DFS/네트워크.cpp
@@ -7,11 +7,8 @@ int visited[200];
 int cnt = 0;
 
 void dfs(int n, vector<vector<int>> computer, int startIdx) {
-
 	visited[startIdx] = -1;
-
 	for (int i = 0;i < n;i++) {
-
 		// 경로있고, 자기자신아니고, 방문 안했던 곳으로 재귀
 		if (computer[startIdx][i] == 1 && i != startIdx && visited[i] == 0) {
 			
