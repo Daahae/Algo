140799fcdf44ac53ed81822513c9af5968fdd5c2
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2020-01-01 22:41:17 +0900

    Update 여행경로.cpp

--- jong4876/20주차 프로그래머스 DFS/여행경로.cpp
+++ jong4876/20주차 프로그래머스 DFS/여행경로.cpp
@@ -1,55 +1,48 @@
-﻿#include <string>
+#include <string>
 #include <vector>
 #include <iostream>
 #include <algorithm>
 
 using namespace std;
 vector<string> answer;
-int visited[10000];
+int visited[10000]; // 미방문 0, 방문 1
 
 
-void clearVisited( int size) {
+void clearVisited(int size) {
 	for (int i = 0;i < size;i++)
 		visited[i] = 0;
-}
-
-void printVec() {
-	for (int i = 0;i < answer.size();i++)
-		cout << answer[i] << " ";
-
-	cout << endl;
 }
 
 
 void DFS(int startIdx, vector<vector<string>> tickets) {
 
 	int destIdx = -1;
-	string destAirport = "ZZZ";
 
-	if (answer.size() >= tickets.size()+1) {
-		return;
-	}
-
-	
 	answer.push_back(tickets[startIdx][1]);
 	visited[startIdx] = 1;
 
-	for(int i=0;i<tickets.size();i++){
+	for (int i = 0;i<tickets.size();i++) {
 		// 도착지 ==> 출발지
 		if (tickets[startIdx][1].compare(tickets[i][0]) == 0
-			&& visited[i]==0 ) {
-			
-			// 사전순 체크
-			if (destAirport.compare(tickets[i][1]) > 0) {
-				destAirport = tickets[i][1];
-				destIdx = i;
-			}
+			&& visited[i] == 0) {
+
+			destIdx = i;
+			DFS(destIdx, tickets);
 		}
 	}
 
-	// 사전순 제일 이른 것으로 재귀
-	if(destIdx !=-1)
-		DFS(destIdx, tickets);
+	// 더 나아갈 경로가 없다면
+	if (destIdx == -1) {
+
+		// 다 돌지 못했다면
+		if (answer.size() < tickets.size() + 1) {
+
+			// 이전 값을 롤백,	 방문체크 취소
+			answer.pop_back();
+			visited[startIdx] = 0;
+		}
+	}
+
 }
 
 
@@ -62,19 +55,20 @@ vector<string> solution(vector<vector<string>> tickets) {
 
 	sort(tickets.begin(), tickets.end());
 
-	// 모든 인덱스에 대해서 DFS가 도는 지 확인?
+	// 모든 인덱스에 대해서 DFS가 도는 지 확인
 	for (int i = 0;i < tickets.size();i++) {
-		answer.clear();
-		clearVisited(size);
-		
+
 		if (tickets[i][0].compare("ICN") == 0) {
-			answer.push_back("ICN");
+			answer.clear();
+			clearVisited(size);
+			answer.push_back(tickets[i][0]);
 			startIdx = i;
+
 			DFS(startIdx, tickets);
 			if (answer.size() == tickets.size() + 1)
 				return answer;
 		}
 	}
-
+	//answer.clear();
 	return answer;
 }
