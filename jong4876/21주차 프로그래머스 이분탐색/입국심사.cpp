bb1e92a74bf3a1a5013114d67bdd483fd55fb663
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2020-01-03 00:44:53 +0900

    incomplete solve : #26 입국심사
    접근방법이 틀린듯

---
+++ jong4876/21주차 프로그래머스 이분탐색/입국심사.cpp
@@ -0,0 +1,37 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(int n, vector<int> times) {
+	long long answer = 0;
+
+	sort(times.begin(), times.end());
+
+	// 처음에 심사받는 사람 제외하고 시작
+	n -= times.size();
+
+	long long presentTime = times[times.size() - 1];
+
+	while (1) {
+
+		for (int i = 0;i < times.size();i++) {
+			if (presentTime% times[i] == 0) {
+				n--;
+				if (n == 0) {
+					presentTime += times[i];
+				}
+					
+
+				break;
+			}
+		}
+	
+		
+		presentTime++;
+	}
+	answer = presentTime;
+
+	return answer;
+}
