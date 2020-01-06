61e5fbc94bf39557ef677114ce7778e2fc04ee59
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2020-01-06 22:17:29 +0900

    incomplete solve : #26 징검다리

---
+++ jong4876/21주차 프로그래머스 이분탐색/징검다리.cpp
@@ -0,0 +1,28 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+int solution(int distance, vector<int> rocks, int n) {
+	int answer = 0;
+	vector<int> diffVec;
+	//dest = 25
+
+	diffVec.push_back(rocks[0]);
+	for (int i = 0;i < rocks.size()-1;i++) {
+		diffVec.push_back(abs(rocks[i + 1] - rocks[i]));
+	}
+	diffVec.push_back(distance - rocks[rocks.size() - 1]);
+	
+	sort(diffVec.begin(), diffVec.end());
+
+	for (int i = 0;i < diffVec.size();i++)
+		cout << diffVec[i] << " ";
+
+	answer = diffVec[n - 1];
+
+	return answer;
+}
