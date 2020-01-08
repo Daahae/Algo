f80f17f5772f326785e952cf39b0119807cdbede
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2020-01-08 17:59:49 +0900

    incomplete solve : #26 다리를 지나는 트럭
    23.1 points

---
+++ jong4876/21주차 프로그래머스 이분탐색/[21주차 추가] 다리를 지나는 트럭.cpp
@@ -0,0 +1,38 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+	int time = 0;
+	queue<int> Q;
+	int sum = 0;
+
+	Q.push(truck_weights[0]);
+
+	for (int i = 0;i < truck_weights.size();i++) {
+		
+		if (sum + truck_weights[i] <= weight) {
+			sum += truck_weights[i];
+			Q.push(truck_weights[i]);
+			time++;
+		}
+		else {
+			while (sum + truck_weights[i] > weight) {
+				sum -= Q.front();
+				Q.pop();
+			}
+			Q.push(truck_weights[i]);
+			sum += truck_weights[i];
+			time += bridge_length;
+		}
+	}
+
+	// 큐가 넘치지 않았다면, 그대로 다리를 통과
+	time += bridge_length;
+
+
+	return time;
+}
