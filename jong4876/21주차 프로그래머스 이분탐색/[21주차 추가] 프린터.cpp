18238565584be1a099894a05507412d9527e49e1
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2020-01-10 23:34:50 +0900

    incomplete solve : #26

---
+++ jong4876/21주차 프로그래머스 이분탐색/[21주차 추가] 프린터.cpp
@@ -0,0 +1,46 @@
+﻿#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+
+
+int solution(vector<int> priorities, int location) {
+	int answer = 0;
+	vector<pair<int, int>> Q; // location, priorities
+	
+	for (int i = 0;i < priorities.size();i++) {
+		Q.push_back(pair<int, int>(i, priorities[i]));
+	}
+
+	while (1) {
+		pair<int, int> frontPair = Q[0];
+		int front = frontPair.second;
+		int flag = 0;
+
+		for (int i = 1;i < Q.size();i++) {
+			if (front < Q[i].second) {
+				flag = 1;
+				break;
+			}
+		}
+		if (flag == 0)
+			break;
+		
+		
+		Q.erase(Q.begin());
+		Q.push_back(frontPair);
+	}
+
+
+	for (int i = 0;i < Q.size();i++) {
+		if (Q[i].first == location)
+			return Q[i].second;
+	}
+
+	
+	return answer;
+}
