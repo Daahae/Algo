2f9fc8c511573ffc40a5870a25e5811e1c47fc7a
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2020-01-14 20:46:49 +0900

    incomplete solve : #27 조이스틱

--- jong4876/22주차 프로그래머스 탐욕법/조이스틱.cpp
+++ jong4876/22주차 프로그래머스 탐욕법/조이스틱.cpp
@@ -1,44 +1,51 @@
-﻿#include <string>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <iostream>
-
-using namespace std;
-
-int solution(vector<int> priorities, int location) {
-	int answer = 0;
-	vector<pair<int, int>> Q; // location, priorities
-
-	for (int i = 0;i < priorities.size();i++) {
-		Q.push_back(pair<int, int>(i, priorities[i]));
-	}
-	int rank = 1;
-	
-	while (!Q.empty()) {
-		pair<int, int> frontPair = Q[0];
-		int front = frontPair.second;
-		int flag = 0;
-
-		for (int i = 1;i < Q.size();i++) {
-			if (front < Q[i].second) {
-				flag = 1;
-				break;
-			}
-		}
-
-		if (flag == 0) {
-			if (frontPair.first == location)
-				return rank;
-			Q.erase(Q.begin());
-			rank++;
-		}
-		else {
-			Q.erase(Q.begin());
-			Q.push_back(frontPair);
-		}
-	}
-
-
-	return rank;
+class Solution {
+    public int solution(String name) {
+        int answer = 0;
+        int answerRight = 0;
+        int answerLeft = 0;
+        int flag =0;
+        for(int j=0;j<name.length();j++) {
+        	if(name.charAt(j) != 'A') {
+        		flag =1;
+        		break;
+        	}
+        }
+        if(flag == 0) // all A
+        	return 0;
+        
+        int i;
+        for(i=0;i<name.length();i++) {
+        	
+        	int upCharGap = (int)name.charAt(i) - (int)'A';
+        	int downCharGap = 26 - upCharGap;
+        	downCharGap = Math.abs(downCharGap);
+            
+        	//System.out.println(upCharGap+ " : "+ downCharGap);
+        	
+        	answerRight += Math.min(upCharGap, downCharGap);
+        	
+            answerRight++;
+        }
+        answerRight--;
+        if(name.charAt(i-1) == 'A')
+        	answerRight--;
+        
+        for(i=name.length();i>0 ;i--) {
+        	
+        	int upCharGap = (int)name.charAt(i%name.length()) - (int)'A';
+        	int downCharGap = 26 - upCharGap;
+        	downCharGap = Math.abs(downCharGap);
+            
+        	System.out.println(upCharGap+ " : "+ downCharGap);
+        	
+        	answerLeft += Math.min(upCharGap, downCharGap);
+            answerLeft++;
+        }
+        answerLeft--;
+        if(name.charAt(i+1) == 'A')
+        	answerLeft--;
+        
+        answer = Math.min(answerRight, answerLeft);
+        return answer;
+    }
 }
