79b0456e92545f52be0027feeaa6f8683a83a06f
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2020-01-21 20:19:29 +0900

    solve : #27 단속카메라
    java 2차원 배열 sort 익히기

--- jong4876/22주차 프로그래머스 탐욕법/단속카메라.java
+++ jong4876/22주차 프로그래머스 탐욕법/단속카메라.java
@@ -2,42 +2,40 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 class Solution {
-    public int solution(int[][] routes) {
-        int answer = 0;
-        
-        
-        // 첫 번째 행렬 기준으로 정렬
-        Arrays.sort(routes, new Comparator<int[]>(){
+	public int solution(int[][] routes) {
+		int answer = 0;
+
+		// 첫 번째 행렬 기준으로 정렬
+		Arrays.sort(routes, new Comparator<int[]>() {
 			@Override
 			public int compare(int[] o1, int[] o2) {
 				return o1[0] - o2[0];
-			}     	
-        });
-        int left =routes[0][0];
-        int right = routes[0][1];
-        
-        
-        for(int i=1;i<routes.length;i++) {
- 
-        	// 범위 좁혀나가기
-			if (left <= routes[i][0] &&right >= routes[i][1]) {
+			}
+		});
+		int left = routes[0][0];
+		int right = routes[0][1];
+
+		for (int i = 1; i < routes.length; i++) {
+
+			// 범위 좁혀나가기
+			if (left <= routes[i][0] && right >= routes[i][1]) {
 				left = routes[i][0];
-				right = routes[i][1];  	
+				right = routes[i][1];
 			}
 
-			else if (left <= routes[i][1] && routes[i][1] <=right)
-				right = routes[i][1];  		
-			
-			else if(right >= routes[i][0] && routes[i][0] >=left)
+			else if (left <= routes[i][1] && routes[i][1] <= right)
+				right = routes[i][1];
+
+			else if (right >= routes[i][0] && routes[i][0] >= left)
 				left = routes[i][0];
-        
+
 			// 범위를 못 좁힌다면 갱신 후 카운트
-        	else {
-        		answer++;
-        		left = routes[i][0];
-        		right = routes[i][1];
-        	}
-        }
-        return answer+1;
-    }
+			else {
+				answer++;
+				left = routes[i][0];
+				right = routes[i][1];
+			}
+		}
+		return answer + 1;
+	}
 }
