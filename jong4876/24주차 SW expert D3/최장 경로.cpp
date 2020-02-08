4412efaddd7472a37e26935b7f756bba438f9ca6
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2020-02-08 21:39:32 +0900

    solve : #29
    벡터의 리스트이용, 다음노드 탐색 유의하기

---
+++ jong4876/24주차 SW expert D3/최장 경로.cpp
@@ -0,0 +1,67 @@
+﻿#include<algorithm>
+#include<iostream>
+#include <vector>
+
+using namespace std;
+
+int resultRoute;
+bool visited[21];
+
+void dfs(vector<int>adj[21], int start, int routeCnt) {
+
+	visited[start] = true;
+	
+	if (resultRoute < routeCnt)
+		resultRoute = routeCnt;
+
+	for (int i = 0;i < adj[start].size();i++) {
+
+		// 다음노드 가리킴 **
+		int nextNode = adj[start][i];
+	
+		if (visited[nextNode] == false) {
+			dfs(adj, nextNode, routeCnt + 1);
+			visited[nextNode] = false;
+		}
+	}
+}
+
+
+int main(int argc, char** argv) {
+	int test_case;
+	int T;
+
+	cin >> T;
+	for (test_case = 1; test_case <= T; ++test_case) {
+
+		int N, M;
+		vector<int>adj[21];
+
+		int maxRouteCnt = 1;
+
+		cin >> N >> M;
+
+		for (int i = 0;i < M;i++) {
+			int start, end;
+			cin >> start >> end;
+
+			adj[start].push_back(end);
+			adj[end].push_back(start);
+		}
+
+		for (int i = 1;i <= N;i++) {
+			resultRoute = 1;
+			for (int j = 1;j <= N;j++)
+				visited[j] = false;
+
+			dfs(adj, i, 1);
+
+			if (maxRouteCnt < resultRoute)
+				maxRouteCnt = resultRoute;
+
+		}
+		cout << "#" << test_case << " " << maxRouteCnt << endl;
+
+	}
+	return 0;
+}
