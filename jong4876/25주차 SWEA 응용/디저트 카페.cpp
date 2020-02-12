a8d8f7c8b104fc2b7e7e89083e3bcc8d6a30ce9a
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2020-02-13 02:56:49 +0900

    Update 디저트 카페.cpp

--- jong4876/25주차 SWEA 응용/디저트 카페.cpp
+++ jong4876/25주차 SWEA 응용/디저트 카페.cpp
@@ -1,4 +1,4 @@
-﻿
+
 #include<iostream>
 #include<vector>
 #include<map>
@@ -12,10 +12,11 @@ int direction; // 우하-2 좌하-1 좌상1 우상2
 int N;
 int start;
 int startX, startY;
+int rowFlag = 0;
 
 
 bool isInArr(int x, int y) {
-	if ((x>=1 && x <= N) && (y>=1 && y <= N))
+	if ((x>=1 && x <= N - rowFlag) && (y>=1 && y <= N - rowFlag))
 		return true;
 
 	return false;
@@ -33,32 +34,28 @@ void dfs(int adj[21][21], int x, int y, int direction, int cnt) {
 	cnt++;
 
 	if (!isInArr(x,y)) {
-		cout << "루트 없음" << endl;
 		maps.clear();
 		return;
 	}
 		
 
 	// 디저트 코스를 중복없이 저장하기 위한 map
-	maps.insert(make_pair(adj[x][y], cnt)); // 디저트종류, 순서 pair
-	cout << x << "," << y << " dir : " <<direction<< endl;
+	maps.insert(make_pair(adj[x][y], cnt)); // (디저트종류, 순서) pair
+	//cout << x << "," << y << " dir : " <<direction<< endl;
 	
 	// 디저트 코스가 중복된 경우
 	if (!isDuplicateDesert(cnt)) {
-
-		if (direction == 2 && startX == x && startY == y) {
-			cout << "코스 성공" << endl;
+		if (direction == 2 && startX == x && startY == y) 
 			return;
-		}
+		
 
-		cout << "코스 중복" << endl;
 		maps.clear();
 		return;
 	}
 
 	// 우하
 	if (direction == -2) {
-		if (isInArr(x + 1, y + 1))
+		if (isInArr(x + 1, y + 1) || cnt == 1)
 			dfs(adj, x + 1, y + 1, -2, cnt);
 
 		else {
@@ -95,11 +92,9 @@ void dfs(int adj[21][21], int x, int y, int direction, int cnt) {
 		
 			// 출발지와 도착지가 다를 때 - 수정 필요
 			if (adj[x][y] != start) {
-				cout << "??" << endl;
 				maps.clear();
 				return;
 			}
-			cout << "dest" << endl;
 		}
 	}
 }
@@ -126,25 +121,31 @@ int main(int argc, char** argv)
 			}
 		}
 		
-		// 나올 수 있는 최대 길이는 (N-1) + (N-1)
-		for (int i = 1;i <= N-1;i++) {
-			for (int j = 1;j <= N-1;j++) {
-				cout << "start :" << i << " " << j << endl;
-				startX = i;
-				startY = j;
+		int size = -1;
+		rowFlag = 0;
+		while (size == -1 && N - rowFlag > 0) {
+			// 끝에 닿지 않아도 돌게만드는 조건 추가
+			for (int i = 1;i <= N - 1;i++) {
+				for (int j = 1;j <= N - 1;j++) {
+					//cout << "start :" << i << " " << j << endl;
+					int row = N - 1;
+					startX = i;
+					startY = j;
 
-				// 현재 인덱스를 출발지로 하여 나올수 있는 경우 계산
-				start = adj[i][j];
-				dfs(adj, i, j,-2,0);
-				int size = maps.size();
-				
-				// 투어를 완성할 수 없는 경우
-				if (size == 0)
-					size = -1;
+					// 현재 인덱스를 출발지로 하여 나올수 있는 경우 계산
+					start = adj[i][j];
+					dfs(adj, i, j, -2, 0);
+					size = maps.size();
 
-				maxVal = max(maxVal, size);
-				maps.clear();
+					// 투어를 완성할 수 없는 경우
+					if (size == 0)
+						size = -1;
+
+					maxVal = max(maxVal, size);
+					maps.clear();
+				}
 			}
+			rowFlag++;
 		}
 		cout << "#" << test_case << " " << maxVal<<endl;
 
