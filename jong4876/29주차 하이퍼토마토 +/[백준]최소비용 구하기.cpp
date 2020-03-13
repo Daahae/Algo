479f5ee07f1aa3fd48f4da669229a1d4e612fbc0
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2020-03-13 23:38:36 +0900

    incom solve : #33
    50 points

--- jong4876/29주차 하이퍼토마토 +/[백준]최소비용 구하기.cpp
+++ jong4876/29주차 하이퍼토마토 +/[백준]최소비용 구하기.cpp
@@ -1,28 +1,63 @@
-﻿#include<iostream>
+#include<iostream>
 #include<algorithm>
 #include<vector>
 #include<algorithm>
+#include<queue>
 
-#define INF 2000001
+#define INF 1000001
 
 using namespace std;
 
-vector<int> vec;
-int adj[1001][1001];
-int N,M;
+vector <pair<int, int>> adj[1001];
+int dp[1001]; // st ~ idx 까지의 최단 경로
+int N, M;
 
-void init() {
-	
-	for (int i = 1;i <= N;i++) {
-		for (int j = 1;j <= N;j++) {
-			adj[i][j] = INF;
+int finSt, finEnd;
+
+void printDp() {
+	for (int i = 1;i <= N;i++)
+		cout << dp[i] << " ";
+	cout << endl;
+
+}
+
+void bfs() {
+	queue<int> Q;
+	priority_queue<pair<int,int>> PQ;
+	Q.push(finSt);
+
+	while (!Q.empty()) {
+		int Qsize = Q.size();
+		
+		for (int j = 0;j < Qsize;j++) {
+			int st = Q.front();
+			Q.pop();
+
+			for (int i = 0;i < adj[st].size();i++) {
+			
+				int next = adj[st][i].first;
+				int nextWeight = adj[st][i].second;
+
+				// 출발 도시에서 st까지의 비용 + st에서 i까지의 비용이 최소인지 확인후 대입
+				if (dp[next] > dp[st] + nextWeight ) {
+					dp[next] = dp[st] + nextWeight;
+					Q.push(next);
+				}
+			}
+			//printDp();
 		}
 	}
 }
 
+void init() {
+
+	for (int i = 1;i <= N;i++) 
+		dp[i] = INF;
+}
+
 int main() {
 
-	cin >> N>>M;
+	cin >> N >> M;
 
 	init();
 
@@ -30,23 +65,14 @@ int main() {
 		int st, end, weight;
 		cin >> st >> end >> weight;
 
-		adj[st][end] = weight;
+		adj[st].push_back(make_pair(end, weight));
 	}
 
-	for (int i = 1;i <= N;i++) {
-		for (int j = 1;j <= N;j++) {
-			for (int k = 1;k <= N;k++) {
-				if (adj[k][j] > adj[k][i] + adj[i][j]) {
-					adj[k][j] = adj[k][i] + adj[i][j];
-				}
-			}
-		}
-	}
+	cin >> finSt >> finEnd;
+	dp[finSt] = 0;
+	bfs();
+	cout << dp[finEnd];
 
-	int destSt, destEnd;
-	cin >> destSt >> destEnd;
-
-	cout << adj[destSt][destEnd];
 
 	return 0;
 }
