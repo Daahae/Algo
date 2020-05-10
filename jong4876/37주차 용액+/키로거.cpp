a68186ba470fc2e36dc2c54ca27ebf242bed8769
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2020-05-10 19:58:30 +0900

    solve : #41 키로거 by 2stack

---
+++ jong4876/37주차 용액+/키로거.cpp
@@ -0,0 +1,61 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	// 해시테이블처럼
+	for (int k = 0;k < N;k++) {
+		string str;
+		int cursorIdx = 0;
+		int strLength = 0;
+		stack<char> s1;
+		stack<char> s2;
+
+		cin >> str;
+
+
+		for (int i = 0;i < str.length();i++) {
+			char order = str[i];
+			if (order == '<') {
+				if (!s1.empty()) {
+					char tmp = s1.top();
+					s1.pop();
+					s2.push(tmp);
+				}
+			}
+			else if (order == '>') {
+				if (!s2.empty()) {
+					char tmp = s2.top();
+					s2.pop();
+					s1.push(tmp);
+				}
+			}
+			else if (order == '-') {
+				if (!s1.empty())
+					s1.pop();
+			}
+			else 
+				s1.push(order);	
+		}
+		// 거꾸로 만드는 과정
+		while (!s1.empty()) {
+			char tmp = s1.top();
+			s1.pop();
+			s2.push(tmp);
+		}
+		while (!s2.empty()) {
+			cout << s2.top();
+			s2.pop();
+		}
+		cout << endl;
+	}
+	
+	return 0;
+}
