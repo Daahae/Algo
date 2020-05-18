045dbb8c6340b866fa3e6d867a2a4a390b2877f6
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2020-05-19 01:33:33 +0900

    solve : #42 binary tree or solution

---
+++ jong4876/38주차 수 정렬하기+/프로그래머스 종이접기.cpp
@@ -0,0 +1,30 @@
+﻿#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+vector<int> answer;
+
+// 사이마다 0, 1을 번갈아 -> 이진트리
+vector<int> solution(int n) {
+
+	answer.push_back(0);
+
+	for (int i = 2;i <= n;i++) {
+
+		vector<int> tmpAnswer = answer;
+		answer.push_back(0);
+
+		for (int j = tmpAnswer.size()-1 ;j >= 0;j--) {	
+			if (tmpAnswer[j] == 1)
+				answer.push_back(0);
+
+			else
+				answer.push_back(1);
+		}
+	}
+	return answer;
+}
+
