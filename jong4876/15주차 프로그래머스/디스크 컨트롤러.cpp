890e35f7ad1eca935b1fb870c85cc07f5c207593
Jonghee Ahn <34762815+jong4876@users.noreply.github.com>
2020-10-31 22:16:17 +0900

    Update 디스크 컨트롤러.cpp

--- jong4876/15주차 프로그래머스/디스크 컨트롤러.cpp
+++ jong4876/15주차 프로그래머스/디스크 컨트롤러.cpp
@@ -1,48 +1,64 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
+
 using namespace std;
 
 bool compare(const pair<int, int> &a, const pair<int, int> &b) {
-	
-	return a.second < b.second;
+
+	if (a.second < b.second)
+		return true;
+
+	else
+		return false;
 
 }
 
-// ** 짧은 작업시간을 가진 프로세스를 먼저 처리한다.
-// presentTime을 1초씩 증가시키면서 해당 presentTime에 맞는 로직을 실행
 int solution(vector<vector<int>> jobs) {
 	int answer = 0;
-	int size;
-	int timeSum = 0;
 	int presentTime = 0;
-	vector<pair<int, int>> primaryQ; //들어온시점, 걸린 시간
+	vector<pair<int, int>> primaryQ;
 
 
-	// 프로세스의 작업시간 기준으로 오름차순 정렬
-	// -> 우선순위 큐 처럼 사용
+	// 작은거 먼저 끝내기 위한 오름차순 정렬 필요
+	for (int i = 0;i < jobs.size();i++) {
+		primaryQ.push_back(pair<int, int>(jobs[i][0], jobs[i][1]));
+	}
 
-	for (int i = 0;i < jobs.size();i++) 
-		primaryQ.push_back(pair<int, int>(jobs[i][0], jobs[i][1])); 
 	sort(primaryQ.begin(), primaryQ.end(), compare);
+	int size = primaryQ.size();
+	int timeSum = 0;
 
-	while (primaryQ.size() > 0) { // 프로세스를 다 실행시킬 때까지
+	while (primaryQ.size() > 0) {
 		int i;
 		int flag = 0;
-		
-		for (i = 0;i < primaryQ.size();i++) {
-			if (presentTime >= primaryQ[i].first) { // 유휴시간 없이 대기하는 프로세스가 있다면
-				presentTime += primaryQ[i].second; // 바로 해당프로세스를 실행
-				timeSum += (presentTime - primaryQ[i].first); // 딜레이 된 시간 역시 더해줌
-				primaryQ.erase(primaryQ.begin() + i); // 실행된 프로세스 제거
+
+		// 현재 진행 가능한 프로세스 중에서 가장 작업시간 작은 프로세스 먼저
+		for (int i = 0;i < primaryQ.size();i++) {
+			if (primaryQ[i].first <= presentTime) {
+
+				// 현재시간부터 작업완료 시간으로 이동
+				presentTime += primaryQ[i].second;
+
+				// 작업시간 + 지연시간 = 작업완료시간 - 시작시간
+				timeSum += (presentTime - primaryQ[i].first);
+
+				// 작업 완료 프로세스 제거
+				primaryQ.erase(primaryQ.begin()+i);
+
+				// 작업이 끝났으면 큐 갱신 후 새로 
 				flag = 1;
 				break;
 			}
 		}
-		if(flag == 0) // 해당presentTime 에 실행할 수 있는 프로세스가 없다면
-			presentTime++;  // 무의미하게 시간을 보냄, 어쩔수없음
+
+		// 실행가능한 프로세스가 없으면 +1초씩
+		if(flag == 0)
+			presentTime++;
 	}
 	answer = timeSum / size;
+
+
+
 	return answer;
 }
